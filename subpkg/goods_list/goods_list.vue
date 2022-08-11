bb18bac9d119b8eeb7cf7b6225a38b566fda8987
king <615721658@qq.com>
2022-08-11 23:56:15 +0800

    完成了商品列表页面的开发

--- subpkg/goods_list/goods_list.vue
+++ subpkg/goods_list/goods_list.vue
@@ -1,7 +1,10 @@
 <template>
   <view>
-    goods_list
-    
+    <view class="goods-list">
+      <view v-for="(goods,i) in goodsList" :key="i" @click="gotoDetail(goods)">
+        <my-goods :goods="goods"></my-goods>
+      </view>
+    </view>
   </view>
 </template>
 
@@ -9,12 +12,82 @@
   export default {
     data() {
       return {
+        //请求参数对象
+        queryObj: {
+          query: '',
+          cid: '',
+          pagenum: 1, //默认请求第1页数据
+          pagesize: 10 //默认一次请求10条数据
+        },
+        //商品列表的数据
+        goodsList: [],
+        total: 0,
+        //通过节流阀防止发起额外的请求
+        //是否正在请求数据
+        isloading:false
         
-      };
+      }
+    },
+    onLoad(options) { //options是页面跳转时所携带的参数
+      //对页面跳转时所携带的参数进行接收
+      this.queryObj.query = options.query || ''
+      this.queryObj.cid = options.cid || ''
+      //console.log(this.queryObj)
+      //调用获取商品数据列表的方法
+      this.getGoodsList()
+    },
+    methods: {
+      //定义获取商品列表数据的方法
+      async getGoodsList(cb) {
+        //打开节流阀
+        this.isloading=true
+        //发起请求
+        const {
+          data: res
+        } = await uni.$http.get('/api/public/v1/goods/search', this.queryObj)
+        if (res.meta.status !== 200) return uni.$showMsg()
+        //关闭节流阀
+        this.isloading=false
+         // 只要数据请求完毕，就立即按需调用 cb 回调函数
+         cb&&cb()//外界可能会传cb也可能不会传  利用短路运算符来判断 如果cb这个值存在就进行调用  调用了就会关闭下拉刷新的效果 
+        // 也就是把uni.stopPullDownRefresh() 这个回调函数调用了一下
+         // 为数据赋值：通过展开运算符的形式，进行新旧数据的拼接
+         this.goodsList=[...this.goodsList,...res.message.goods]
+         this.total=res.message.total
+         
+      },
+      //点击item项(这里是goods)跳转到商品的详情页
+      gotoDetail(goods){
+        uni.navigateTo({
+          url:'/subpkg/goods_detail/goods_detail?goods_id='+goods.goods_id
+        })
+      },
+    },
+    //用来监听页面的上拉触底行为
+    onReachBottom() {
+      // 判断是否还有下一页数据
+      if(this.queryObj.pagenum * this.queryObj.pagesize>=this.total) return uni.$showMsg('数据加载完毕!')
+      // 判断是否正在请求其它数据，如果是，则不发起额外的请求
+      if(this.isloading) return
+      //让页码值自增+1
+      this.queryObj.pagenum+=1
+      //页码值自增加1后 重新获取列表数据
+      this.getGoodsList()
+    },
+    // 下拉刷新的事件
+    onPullDownRefresh() {
+      // 1. 重置关键数据
+      this.queryObj.pagenum = 1
+      this.total = 0
+      this.isloading = false
+      this.goodsList = []
+      // 2. 重新发起请求
+      this.getGoodsList(() => uni.stopPullDownRefresh())
     }
+   
   }
 </script>
 
 <style lang="scss">
-
+  
 </style>
---
+++ uni_modules/my-goods/components/my-goods/my-goods.vue
@@ -0,0 +1,76 @@
+<template>
+  <view class="goods-item">
+    <!-- 商品左侧图片区域-->
+    <view class="goods-item-left">
+      <image :src="goods.goods_small_logo || defaultPic" class="goods-pic"></image>
+    </view>
+    <!-- 商品右侧信息区域-->
+    <view class="goods-item-right">
+      <!-- 商品标题-->
+      <view class="goods-name">{{goods.goods_name}}</view>
+      <view class="goods-info-box">
+        <!-- 商品价格-->
+        <view class="goods-price">￥{{goods.goods_price | tofixed}}</view><!-- 利用管道符调用 过滤器 -->
+      </view>
+    </view>
+  </view>
+</template>
+<script>
+  export default{
+    props:{
+      //自定义属性
+      goods:{
+        type:Object,
+        default:{}
+      }
+    },
+    data(){
+      return{
+        //默认的空图片
+        defaultPic: 'https://img3.doubanio.com/f/movie/8dd0c794499fe925ae2ae89ee30cd225750457b4/pics/movie/celebrity-default-medium.png'
+      };
+    },
+    filters:{
+      //定义一个过滤器
+      tofixed(num){
+        return Number(num).toFixed(2)//转成数字型保留两位小数
+        
+      }
+    }
+  }
+</script>
+<style lang="scss">
+  .goods-item {
+    display: flex;
+    padding: 10px 5px;
+    border-bottom: 1px solid #f0f0f0;
+  
+    .goods-item-left {
+      margin-left: 5px;
+  
+      .goods-pic {
+        width: 100px;
+        height: 100px;
+        display: flex;
+      }
+  
+    }
+  
+    .goods-item-right {
+      display: flex;
+      flex-direction: column;
+      justify-content: space-between;
+  
+      .goods-name {
+        font-size: 13px
+      }
+       .goods-info-box{
+         .goods-price {
+           font-size: 16px;
+           color: #c00000;
+         }
+       }
+      
+    }
+  }
+</style>
