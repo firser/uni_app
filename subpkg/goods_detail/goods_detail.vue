2ba062523d74113f8bdc57963f22a9780802d5da
king <615721658@qq.com>
2022-08-12 16:49:34 +0800

    完成了商品详情页面的开发

--- subpkg/goods_detail/goods_detail.vue
+++ subpkg/goods_detail/goods_detail.vue
@@ -1,6 +1,35 @@
 <template>
-  <view>
-    
+  <view v-if="goods_info.goods_name" class="goods-detail-container">
+    <swiper :indicator-dots="true" :autoplay="true" :interval="3000" :duration="1000" :circular="true">
+      <swiper-item v-for="(item,i) in goods_info.pics" :key="i">
+        <!-- 实现放大预览效果 把当前点击的图片的索引，传递到 preview() 处理函数中 -->
+        <image :src="item.pics_big" @click="preview(i)"></image>
+      </swiper-item>
+    </swiper>
+    <!-- 商品信息区域-->
+    <view class="goods-info-box">
+      <!-- 商品价格 -->
+      <view class="price">￥{{goods_info.goods_price}}</view>
+      <!-- 信息主体区域 -->
+      <view class="goods-info-body">
+        <!-- 商品名称 -->
+        <view class="goods-name">{{goods_info.goods_name}}</view>
+        <!-- 收藏 -->
+        <view class="favi">
+          <uni-icons type="star" size="18" color="gray"></uni-icons>
+          <text>收藏</text>
+        </view>
+      </view>
+      <!-- 运费 -->
+      <view class="yf">快递：免运费</view>
+    </view>
+    <!-- 富文本组件用来处理数据里面的html标签等-->
+    <rich-text :nodes="goods_info.goods_introduce"></rich-text>
+    <!-- 商品导航组件区域-->
+    <view class="goods_nav">
+      <uni-goods-nav :fill="true" :options="options" :buttonGroup="buttonGroup" @click="onClick" @buttonClick="buttonClick" />
+    </view>
+
   </view>
 </template>
 
@@ -8,12 +37,140 @@
   export default {
     data() {
       return {
-        
+        //商品详情对象
+        goods_info: {},
+        // 左侧按钮组的配置对象
+        options: [{
+          icon: 'shop',
+          text: '店铺'
+        }, {
+          icon: 'cart',
+          text: '购物车',
+          info: 2
+        }],
+        // 右侧按钮组的配置对象
+        buttonGroup: [{
+            text: '加入购物车',
+            backgroundColor: '#ff0000',
+            color: '#fff'
+          },
+          {
+            text: '立即购买',
+            backgroundColor: '#ffa200',
+            color: '#fff'
+          }
+        ]
+
       };
+    },
+    onLoad(options) {
+      //获取商品id
+      const goods_id = options.goods_id
+      // 调用请求商品详情数据的方法
+      this.getGoodsDetail(goods_id)
+    },
+    methods: {
+      // 定义请求商品详情数据的方法
+      async getGoodsDetail(goods_id) {
+        const {
+          data: res
+        } = await uni.$http.get('/api/public/v1/goods/detail', {
+          goods_id: goods_id
+        })
+        if (res.meta.status !== 200) return uni.$showMsg()
+        // 使用字符串的 replace() 方法，为 img 标签添加行内的 style 样式，从而解决图片底部空白间隙的问题
+        //  // 使用字符串的 replace() 方法，将 webp 的后缀名替换为 jpg 的后缀名      解决 .webp 格式图片在 ios 设备上无法正常显示的问题：
+        res.message.goods_introduce = res.message.goods_introduce.replace(/<img /g, '<img style="display:block;"')
+          .replace(/webp/g, 'jpg')
+        //赋值
+        this.goods_info = res.message
+      },
+      //实现轮播图的预览效果
+      preview(i) {
+        // 调用 uni.previewImage() 方法预览图片
+        uni.previewImage({
+          //预览时  默认显示图片的索引
+          current: i,
+          // 所有图片 url 地址的数组(urls)
+          urls: this.goods_info.pics.map(x => x.pics_big) //没循环一次获得一个x  然后返回x上的pics_big属性
+        })
+      },
+      //点击商品导航组件左侧的按钮，会触发 uni-goods-nav 的 @click 事件处理函数
+      //定义左侧按钮的点击事件处理函数
+      onClick(e) {
+        if(e.content.text==='购物车'){
+         uni.switchTab({
+           url:'/pages/cart/cart'
+         })
+        }
+      }
     }
   }
 </script>
 
 <style lang="scss">
+  //轮播图样式
+  swiper {
+    height: 750rpx;
 
+    image {
+      width: 100%;
+      height: 100%;
+
+    }
+  }
+
+  //商品区域样式
+  .goods-info-box {
+    padding: 10px;
+    padding-right: 0;
+
+    .price {
+      color: #c00000;
+      font-size: 18px;
+      margin: 10px 0;
+
+    }
+
+    .goods-info-body {
+      display: flex;
+      justify-content: space-between;
+
+      .goods-name {
+        font-size: 13px;
+        margin-right: 10px;
+
+      }
+
+      .favi {
+        width: 120px;
+        font-size: 12px;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+        border-left: 1px solid #efefef;
+        color: gray
+      }
+
+    }
+
+    .yf {
+      font-size: 12px;
+      color: gray;
+      margin: 10px 0;
+
+    }
+  }
+  //商品导航组件样式
+  .goods_nav{
+    // 为商品导航组件添加固定定位
+    position: fixed;
+    bottom: 0;
+    left: 0;
+    width: 100%;
+  }
+  .goods-detail-container{
+    padding-bottom: 50px;
+  }
 </style>
